961fa5bce09f9a4d65b5709c10307e6353da6922
Christian Wansart <cwansart@users.noreply.github.com>
2016-03-26 21:16:20 +0100

    Fixed missing error messages bug

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,17 +11,25 @@
 |
 */
 
+
+
+// As of Laravel 5.2 you need the web middlware for auth and stuff,
+// but there seems to be a bug that adds it two times, if you enclose
+// your routes with them. That causes the error message to get lost.
+// For now we just removed the group.
+//Route::group(['middleware' => ['web']], function () {
+
+Route::auth();
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['middleware' => 'web'], function () {
-    Route::auth();
+Route::get('/attendees/create', 'AttendeeController@create');
 
-    Route::get('/attendees/create', 'AttendeeController@create');
+Route::resource('quizzes', 'QuizController');
+Route::resource('questions', 'QuestionController');
+Route::resource('categories', 'CategoryController');
 
-    Route::resource('quizzes', 'QuizController');
-    Route::resource('questions', 'QuestionController');
-    Route::resource('categories', 'CategoryController');
-});
+//});
 
