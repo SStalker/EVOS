17d233084ac8ba1b6463a402820adf236f27a10a
Christian Wansart <cwansart@users.noreply.github.com>
2016-04-10 12:40:35 +0200

    Improved showQuestion view

--- resources/views/questions/showQuestion.blade.php
+++ resources/views/questions/showQuestion.blade.php
@@ -1,3 +1,11 @@
-{!! $question->question !!}
+@extends('layouts.quizbackend')
 
-<a class="btn btn-primary" style="margin-top: -7px;" href="{!! url('/quizzes/'.$question->quiz->id.'/next') !!}">Nächste Frage</a>
+@section('title', 'Bitte anmelden!')
+
+@section('content')
+
+    {!! $question->question !!}
+
+    <a class="btn btn-primary next-button" href="{!! action('QuizController@next', [$question->quiz->category->id, $question->quiz->id]) !!}">Nächste Frage</a>
+
+@endsection
