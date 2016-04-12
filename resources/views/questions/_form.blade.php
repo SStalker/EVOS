c8aae6e1517e6ed6e5c4a53a7da0af0e240130d1
lkschenk <larissa.schenk@hotmail.de>
2016-04-12 11:07:01 +0200

    Added "Vorschau" and "Bild anhängen" Buttons
    ..in question-view.Yet without functionality.

--- resources/views/questions/_form.blade.php
+++ resources/views/questions/_form.blade.php
@@ -1,23 +1,28 @@
 <div class="panel-body">
     <div class="form-group">
         <label for="question">Frage</label>
-        {!! Form::text('question', null, ['class' => 'form-control']) !!}
+        {!! Form::text('question', null, ['class' => 'form-control']) !!} <br>
+        <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a> <a class="btn btn-default" style="margin-top: -7px;" href="">Bild anhängen</a>
     </div>
     <div class="form-group">
         <label for="answerA">Antwort 1</label>
-        {!! Form::textarea('answerA', null, ['class' => 'form-control', 'rows' => 2]) !!}
+        {!! Form::textarea('answerA', null, ['class' => 'form-control', 'rows' => 2]) !!}<br>
+        <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a> <a class="btn btn-default" style="margin-top: -7px;" href="">Bild anhängen</a>
     </div>
     <div class="form-group">
         <label for="answerB">Antwort 2</label>
-        {!! Form::textarea('answerB', null, ['class' => 'form-control', 'rows' => 2]) !!}
+        {!! Form::textarea('answerB', null, ['class' => 'form-control', 'rows' => 2]) !!}<br>
+        <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a> <a class="btn btn-default" style="margin-top: -7px;" href="">Bild anhängen</a>
     </div>
     <div class="form-group">
         <label for="answerC">Antwort 3</label>
-        {!! Form::textarea('answerC', null, ['class' => 'form-control', 'rows' => 2]) !!}
+        {!! Form::textarea('answerC', null, ['class' => 'form-control', 'rows' => 2]) !!}<br>
+        <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a> <a class="btn btn-default" style="margin-top: -7px;" href="">Bild anhängen</a>
     </div>
     <div class="form-group">
         <label for="answerD">Antwort 4</label>
-        {!! Form::textarea('answerD', null, ['class' => 'form-control', 'rows' => 2]) !!}
+        {!! Form::textarea('answerD', null, ['class' => 'form-control', 'rows' => 2]) !!}<br>
+        <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a> <a class="btn btn-default" style="margin-top: -7px;" href="">Bild anhängen</a>
     </div>
     <div class="form-group">
         <label for="countdown">Countdown</label>
