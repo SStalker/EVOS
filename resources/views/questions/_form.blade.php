d7c7ae96d59a27c4c1609f61ce4add4358d183a2
lkschenk <larissa.schenk@hotmail.de>
2016-04-12 15:19:43 +0200

    changed some little styles

--- resources/views/questions/_form.blade.php
+++ resources/views/questions/_form.blade.php
@@ -1,9 +1,9 @@
 <div class="panel-body">
     <div class="form-group">
         <label for="question">Frage</label>
-        {!! Form::textarea('question', null, ['class' => 'form-control', 'rows' => 2]) !!} <br>
+        {!! Form::textarea('question', null, ['class' => 'form-control', 'rows' => 3]) !!} <br>
         <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a>
-        <label class="btn btn-default" for="file-selector">
+        <label class="btn btn-default" for="file-selector" style="margin-top:-7px">
             <input id="file-selector" type="file" style="display:none;">
             Bild anhängen
         </label>
@@ -12,7 +12,7 @@
         <label for="answerA">Antwort 1</label>
         {!! Form::text('answerA', null, ['class' => 'form-control']) !!}<br>
         <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a>
-        <label class="btn btn-default" for="file-selector">
+        <label class="btn btn-default" for="file-selector" style="margin-top:-7px">
             <input id="file-selector" type="file" style="display:none;">
             Bild anhängen
         </label>
@@ -21,7 +21,7 @@
         <label for="answerB">Antwort 2</label>
         {!! Form::text('answerB', null, ['class' => 'form-control']) !!}<br>
         <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a>
-        <label class="btn btn-default" for="file-selector">
+        <label class="btn btn-default" for="file-selector" style="margin-top:-7px">
             <input id="file-selector" type="file" style="display:none;">
             Bild anhängen
         </label>
@@ -30,7 +30,7 @@
         <label for="answerC">Antwort 3</label>
         {!! Form::text('answerC', null, ['class' => 'form-control']) !!}<br>
         <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a>
-        <label class="btn btn-default" for="file-selector">
+        <label class="btn btn-default" for="file-selector" style="margin-top:-7px">
             <input id="file-selector" type="file" style="display:none;">
             Bild anhängen
         </label>
@@ -39,7 +39,7 @@
         <label for="answerD">Antwort 4</label>
         {!! Form::text('answerD', null, ['class' => 'form-control']) !!}<br>
         <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a>
-        <label class="btn btn-default" for="file-selector">
+        <label class="btn btn-default" for="file-selector" style="margin-top:-7px">
             <input id="file-selector" type="file" style="display:none;">
             Bild anhängen
         </label>
