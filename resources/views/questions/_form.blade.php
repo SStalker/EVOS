f0a5850267feefebf57d9faa902a87095a81e30d
lkschenk <larissa.schenk@hotmail.de>
2016-04-12 15:12:42 +0200

    included upload dialogue for images

--- resources/views/questions/_form.blade.php
+++ resources/views/questions/_form.blade.php
@@ -2,27 +2,47 @@
     <div class="form-group">
         <label for="question">Frage</label>
         {!! Form::textarea('question', null, ['class' => 'form-control', 'rows' => 2]) !!} <br>
-        <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a> <a class="btn btn-default" style="margin-top: -7px;" href="">Bild anhängen</a>
+        <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a>
+        <label class="btn btn-default" for="file-selector">
+            <input id="file-selector" type="file" style="display:none;">
+            Bild anhängen
+        </label>
     </div>
     <div class="form-group">
         <label for="answerA">Antwort 1</label>
         {!! Form::text('answerA', null, ['class' => 'form-control']) !!}<br>
-        <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a> <a class="btn btn-default" style="margin-top: -7px;" href="">Bild anhängen</a>
+        <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a>
+        <label class="btn btn-default" for="file-selector">
+            <input id="file-selector" type="file" style="display:none;">
+            Bild anhängen
+        </label>
     </div>
     <div class="form-group">
         <label for="answerB">Antwort 2</label>
         {!! Form::text('answerB', null, ['class' => 'form-control']) !!}<br>
-        <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a> <a class="btn btn-default" style="margin-top: -7px;" href="">Bild anhängen</a>
+        <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a>
+        <label class="btn btn-default" for="file-selector">
+            <input id="file-selector" type="file" style="display:none;">
+            Bild anhängen
+        </label>
     </div>
     <div class="form-group">
         <label for="answerC">Antwort 3</label>
         {!! Form::text('answerC', null, ['class' => 'form-control']) !!}<br>
-        <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a> <a class="btn btn-default" style="margin-top: -7px;" href="">Bild anhängen</a>
+        <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a>
+        <label class="btn btn-default" for="file-selector">
+            <input id="file-selector" type="file" style="display:none;">
+            Bild anhängen
+        </label>
     </div>
     <div class="form-group">
         <label for="answerD">Antwort 4</label>
         {!! Form::text('answerD', null, ['class' => 'form-control']) !!}<br>
-        <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a> <a class="btn btn-default" style="margin-top: -7px;" href="">Bild anhängen</a>
+        <a class="btn btn-primary" style="margin-top: -7px;" href="">Vorschau</a>
+        <label class="btn btn-default" for="file-selector">
+            <input id="file-selector" type="file" style="display:none;">
+            Bild anhängen
+        </label>
     </div>
     <div class="form-group">
         <label for="countdown">Countdown</label>
