b1573d0dfae153cf389253b6f9279a39412e4bb0
DaHerzog <DaHerzog@users.noreply.github.com>
2016-04-26 10:32:21 +0200

    Fixed typo

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,7 @@
 
 
 
-// As of Laravel 5.2 you need the web middlware for auth and stuff,
+// As of Laravel 5.2 you need the web middleware for auth and stuff,
 // but there seems to be a bug that adds it two times, if you enclose
 // your routes with them. That causes the error message to get lost.
 // For now we just removed the group.
