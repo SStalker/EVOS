e550707a22ee9a68d928434eb269ea2117959904
DaHerzog <DaHerzog@users.noreply.github.com>
2016-04-29 13:15:51 +0200

    Added some styling

--- resources/views/frontend/entrypoint.blade.php
+++ resources/views/frontend/entrypoint.blade.php
@@ -3,17 +3,17 @@
 @section('frontEndContent')
 
     <div id="enterQuizPanel" class="container">
-        <img src="images/evos.png" class="img-responsive">
+        <img src="images/evos.png" class="img-responsive center-block" style="margin-bottom: 10%">
         <label class="control-label">Quiz PIN eingeben: </label>
         <input id='quizPinInput' class="form-control">
-        <button id='quizPinBtn' class="btn btn-default">Okay</button>
+        <button id='quizPinBtn' class="btn btn-default center-block">Okay</button>
     </div>
 
     <div id ="enterNamePanel" class="container" style="display: none;">
-        <img src="images/evos.png" class="img-responsive">
+        <img src="images/evos.png" class="img-responsive center-block" style="margin-bottom: 10%">
         <label class="control-label">Namen eingeben: </label>
         <input id='enterNameInput' class="form-control">
-        <button id='enterNameBtn' class="btn btn-default">Okay</button>
+        <button id='enterNameBtn' class="btn btn-default center-block">Okay</button>
     </div>
 
     <div id ="waitingPanel" class="container" style="display: none;">
