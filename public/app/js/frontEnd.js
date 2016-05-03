19e653763365f65ddb8b06a757253292d50309fd
Lukas Hannigbrinck <lukas@bluepanel.org>
2016-05-03 13:30:43 +0200

    create initial code for websocket usage

--- public/app/js/frontEnd.js
+++ public/app/js/frontEnd.js
@@ -2,7 +2,48 @@
  * Created by davidherzog on 09.04.16.
  */
 
+var url = "127.0.0.1";
+var websocket = new WebSocket("ws://"+ url +":8080/EVOS-Sync/sync");
+var websocketOk;
+websocket.onopen = function(event){
+    websocketOk = true;
+    //DEBUG
+    console.log("open");
+};
+
+websocket.onerror = function (error) {
+
+    //If the websocket is closed due some issues set websocketOk to false
+    websocketOk = false;
+    //DEBUG
+    console.log('WebSocket Error');
+};
+
+//receive messages from server
+websocket.onmessage = function(event){
+    //process the received server data
+    processMessage(event.data);
+};
+
+/* parse/process incoming sync server messages */
+function processMessage(data){
+    //DEBUG
+    console.log(data);
+    
+    switch (data.type){
+        case 'logon': processLogon(data);
+            break;
+        default: console.log('default');
+    }
+};
+
+function processLogon(data){
+    console.log('processLogon')
+}
+
+
 $(document).ready(function() {
+    var quizPin;
     var jqXhr;
     $('#quizAlert').on('click', function() {
         $('#quizAlert').toggleClass('in');
@@ -11,7 +52,7 @@ $(document).ready(function() {
 
     $('#quizPinBtn').on('click', function(e) {
         
-        var quizPin = $('#quizPinInput').val();
+        quizPin = $('#quizPinInput').val();
         jqXhr = $.ajax('/quiz/'+quizPin)
             .done(function(response) {
             
@@ -47,11 +88,21 @@ $(document).ready(function() {
             }
         }).done(function(response) {
             if (response == 'waiting') {
+                
                 $('#enterNamePanel').fadeOut(400, function() {
                     $('#waitingPanel').fadeIn(400);
                 });
+
+                var data = {
+                    type: 'logon',
+                    quiz_id: quizPin,
+                    nickname: name
+                }
+
+                websocket.send(JSON.stringify(data));
+
             } else {
-                
+                console.log(response);
             }
         })
     })
