e6e7fb8436cf8c6a0f2dbd96b7a6e3f1956e573c
Christian Wansart <cwansart@users.noreply.github.com>
2016-05-03 20:23:31 +0200

    Fixed some layout issues

--- resources/views/questions/start.blade.php
+++ resources/views/questions/start.blade.php
@@ -3,25 +3,6 @@
 @section('title', 'Bitte anmelden!')
 
 @section('content')
-
-        <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>@yield('title')</title>
-
-        <!-- Fonts -->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
-        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
-
-        <!-- Styles -->
-        <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
-    </head>
-
-    <body id="app-layout">
     <nav class="navbar navbar-default navbar-static-top">
         <div class="container">
             <div class="navbar-header">
@@ -76,7 +57,6 @@
             <h3>Antworten: <span id="answer-count">0</span></h3>
         </div>
     </div>
-</body>
 
     <script>
         $(function() {
