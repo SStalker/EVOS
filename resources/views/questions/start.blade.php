11ac2ab3e8e04f09734694e1eb27aa39649b1b20
Christian Wansart <cwansart@users.noreply.github.com>
2016-05-10 09:58:52 +0200

    Hide next button while countdown. Closed #60

--- resources/views/questions/start.blade.php
+++ resources/views/questions/start.blade.php
@@ -243,7 +243,7 @@
                 $('#answerB').removeClass("bg-success bg-danger");
                 $('#answerC').removeClass("bg-success bg-danger");
                 $('#answerD').removeClass("bg-success bg-danger");
-                $('#next-button').attr('disabled', 'disabled');
+                $('#next-button').hide();
 
                 // For every new question the server must be informed
                 question(ws);
@@ -266,7 +266,7 @@
                     // Enable next-button after the current question has finished.
                     // Shows the correct answers
                     setInterval(function () {
-                        $('#next-button').removeAttr('disabled');
+                        $('#next-button').show();
 
                         correctAnswers.a ? $('#answerA').addClass("bg-success") : $('#answerA').addClass("bg-danger");
                         correctAnswers.b ? $('#answerB').addClass("bg-success") : $('#answerB').addClass("bg-danger");
