bc6b2fef97e7a93a57bc4a8a9a0ab127a222df1f
Lukas Hannigbrinck <lukas@bluepanel.org>
2016-05-14 15:23:31 +0200

    Build and styled answer buttons

--- resources/views/frontend/entrypoint.blade.php
+++ resources/views/frontend/entrypoint.blade.php
@@ -1,48 +1,58 @@
 @extends('layouts.frontend')
 
 @section('frontEndContent')
+    <div class="wrapper">
+        <div id="enterQuizPanel" class="container">
+            <img src="images/evos.png" class="img-responsive center-block" style="margin-bottom: 10%">
+            <label class="control-label">Quiz PIN eingeben: </label>
+            <input id='quizPinInput' class="form-control">
+            <button id='quizPinBtn' class="btn btn-default center-block">Okay</button>
 
-    <div id="enterQuizPanel" class="container">
-        <img src="images/evos.png" class="img-responsive center-block" style="margin-bottom: 10%">
-        <label class="control-label">Quiz PIN eingeben: </label>
-        <input id='quizPinInput' class="form-control">
-        <button id='quizPinBtn' class="btn btn-default center-block">Okay</button>
-
-        <div class="alert alert-danger fade out text-center" id="quizAlert" style="margin-top: 15%">
-            Dieses Quiz existiert nicht.
+            <div class="alert alert-danger fade out text-center" id="quizAlert" style="margin-top: 15%">
+                Dieses Quiz existiert nicht.
+            </div>
         </div>
-    </div>
 
-    <div id ="enterNamePanel" class="container" style="display: none;">
-        <img src="images/evos.png" class="img-responsive center-block" style="margin-bottom: 10%">
-        <label class="control-label">Namen eingeben: </label>
-        <input id='enterNameInput' class="form-control">
-        <button id='enterNameBtn' class="btn btn-default center-block">Okay</button>
+        <div id ="enterNamePanel" class="container" style="display: none;">
+            <img src="images/evos.png" class="img-responsive center-block" style="margin-bottom: 10%">
+            <label class="control-label">Namen eingeben: </label>
+            <input id='enterNameInput' class="form-control">
+            <button id='enterNameBtn' class="btn btn-default center-block">Okay</button>
 
-        <div class="alert alert-danger fade out text-center" id="nameAlert" style="margin-top: 15%">
-            Name konnte nicht eingetragen werden.
+            <div class="alert alert-danger fade out text-center" id="nameAlert" style="margin-top: 15%">
+                Name konnte nicht eingetragen werden.
+            </div>
         </div>
-    </div>
 
-    <div id ="waitingPanel" class="container" style="display: none;">
-        <img src="images/evos.png" class="img-responsive">
-        <p>Und jetzt warten wir mal.....</p>
-    </div>
+        <div id ="waitingPanel" class="container" style="display: none;">
+            <img src="images/evos.png" class="img-responsive">
+            <p>Und jetzt warten wir mal.....</p>
+        </div>
 
-    <div id="questionPanel" class="container-fluid" style="display: none">
-        <div class="row">
-            <div id="answerA" class="col-md-6">
-                <span> A </span>
-            </div>
-            <div id="answerB" class="col-md-6">
-                <span> B </span>
-            </div>
-            <div class="clearfix"></div>
-            <div id="answerC" class="col-md-6">
-                <span> C </span>
-            </div>
-            <div id="answerD" class="col-md-6">
-                <span> D </span>
+        <div id="questionPanel" class="container-fluid" style="display: none">
+            <img src="images/evos.png" class="img-responsive logo">
+            <div class="row">
+                <div  class="col-md-6 ">
+                    <div id="answerA" data-value="a" class="answer panel panel-default" >
+                        <div class="panel-body"> A </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div id="answerB" data-value="b" class="answer panel panel-default" >
+                        <div class="panel-body"> B </div>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+                <div  class="col-md-6 ">
+                    <div id="answerC" data-value="c" class="answer panel panel-default" >
+                        <div class="panel-body"> C </div>
+                    </div>
+                </div>
+                <div  class="col-md-6 ">
+                    <div id="answerD" data-value="d" class="answer panel panel-default" >
+                        <div class="panel-body"> D </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
