dba123e287f468dffd00885d26f8edc4f28199a3
sstalker <raphael@bluepanel.org>
2016-05-31 10:32:35 +0200

    Close #91

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,7 +19,15 @@
 // For now we just removed the group.
 //Route::group(['middleware' => ['web']], function () {
 
-Route::auth();
+// Authentication Routes...
+Route::get('login', 'Auth\AuthController@showLoginForm');
+Route::post('login', 'Auth\AuthController@login');
+Route::get('logout', 'Auth\AuthController@logout');
+
+// Password Reset Routes...
+Route::get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
+Route::post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
+Route::post('password/reset', 'Auth\PasswordController@reset');
 
 Route::get('/', function () {
     return view('welcome');
