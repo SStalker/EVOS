3a8d6de5f33e5166432bc48b2176bacd33cf0911
Lukas Hannigbrinck <lukas@bluepanel.org>
2016-06-04 13:27:25 +0200

    Closes #97

--- resources/views/frontend/entrypoint.blade.php
+++ resources/views/frontend/entrypoint.blade.php
@@ -3,7 +3,7 @@
 @section('frontEndContent')
     <div class="wrapper">
         <div id="enterQuizPanel" class="container">
-            <img src="images/evos.png" class="img-responsive center-block" style="margin-bottom: 10%">
+            <img src="{{ asset('images/evos.png') }}" class="img-responsive center-block" style="margin-bottom: 10%">
             <label class="control-label">Quiz PIN eingeben: </label>
             <input id='quizPinInput' class="form-control">
             <button id='quizPinBtn' class="btn btn-default center-block">Okay</button>
@@ -14,7 +14,7 @@
         </div>
 
         <div id ="enterNamePanel" class="container" style="display: none;">
-            <img src="images/evos.png" class="img-responsive center-block" style="margin-bottom: 10%">
+            <img src="{{ asset('images/evos.png') }}" class="img-responsive center-block" style="margin-bottom: 10%">
             <label class="control-label">Namen eingeben: </label>
             <input id='enterNameInput' class="form-control">
             <button id='enterNameBtn' class="btn btn-default center-block">Okay</button>
@@ -25,12 +25,12 @@
         </div>
 
         <div id ="waitingPanel" class="container" style="display: none;">
-            <img src="images/evos.png" class="img-responsive">
+            <img src="{{ asset('images/evos.png') }}" class="img-responsive">
             <p>Und jetzt warten wir mal.....</p>
         </div>
 
         <div id="questionPanel" class="container-fluid" style="display: none">
-            <img src="images/evos.png" class="img-responsive logo">
+            <img src="{{ asset('images/evos.png') }}" class="img-responsive logo">
             <div class="row">
                 <div  class="col-md-6 ">
                     <div id="answerA" data-value="a" class="answer panel panel-default" >
@@ -57,7 +57,7 @@
         </div>
 
         <div id="endQuizPanel" class="container" style="display: none; text-align: center;">
-            <img src="images/evos.png" class="img-responsive center-block logo">
+            <img src="{{ asset('images/evos.png') }}" class="img-responsive center-block logo">
             <p style="margin-top: 2%">Das Quiz ist zu Ende!</p>
             <button id='startNewBtn' class="btn btn-default center-block" style="margin-top: 2%">Neue PIN eingeben!</button>
         </div>
