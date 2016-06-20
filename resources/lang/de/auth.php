dcc03ca17ce26b30ef743df126c1972536166cf3
Christian Wansart <cwansart@users.noreply.github.com>
2016-06-20 14:05:15 +0200

    Added some German validation translations.

---
+++ resources/lang/de/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Die eingegebenen Daten stimmen nicht mit unseren überein.',
+    'throttle' => 'Zu viele Anmeldeversuche. Bitte versuche es noch einmal in :seconds Sekunden.',
+
+];
---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Passwörter müssen mindestens 6 Zeichen lang sein und der Wiederholung übereinstimmen!',
+    'reset' => 'Dein Passwort wurde zurückgesetzt!',
+    'sent' => 'Dir wurde ein Zurücksetzen-Link per E-Mail zugeschickt!',
+    'token' => 'Das Zurücksetzen-Token ist ungültig!',
+    'user' => "Wir konnten keinen Nutzer mit der E-Mail-Adresse finden.",
+
+];
