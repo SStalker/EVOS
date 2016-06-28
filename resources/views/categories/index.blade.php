b5dcb4bb299374ea496195cc3b5f19b584df8bf1
Christian Wansart <cwansart@users.noreply.github.com>
2016-06-28 14:04:20 +0200

    Added tooltips

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -4,48 +4,63 @@
 
 @section('content')
 
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <div class="pull-right">
-            <a class="btn btn-primary" style="margin-top: -7px;" href="{{ action('CategoryController@create') }}">Kategorie erstellen</a>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <div class="pull-right">
+                <a class="btn btn-primary" style="margin-top: -7px;" href="{{ action('CategoryController@create') }}"
+                   data-toggle="tooltip"
+                   data-placement="left" title="Erstellt eine neue Kategorie.">Kategorie erstellen</a>
+            </div>
+
+            Kategorien
         </div>
 
-        Kategorien
-    </div>
+        <div class="panel-body">
+            <div class="table-responsive">
+                @if($categories->count() > 0)
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>Titel</th>
+                            <th>Besitzer</th>
+                            <th style="width:30%">Aktionen</th>
+                        </tr>
+                        </thead>
+                        <tbody class="table-hover">
+                        @foreach($categories as $category)
+                            <tr>
+                                <td>
+                                    <a href="{{ action('CategoryController@show', [$category->id]) }}">{{ $category->title }}</a>
+                                </td>
+                                <td>{{ $category->user->name }}</td>
+                                <td>
 
-    <div class="panel-body">
-        <div class="table-responsive">
-            @if($categories->count() > 0)
-            <table class="table">
-                <thead>
-                <tr>
-                    <th>Titel</th>
-                    <th>Besitzer</th>
-                    <th style="width:30%">Aktionen</th>
-                </tr>
-                </thead>
-                <tbody class="table-hover">
-                @foreach($categories as $category)
-                    <tr>
-                        <td><a href="{{ action('CategoryController@show', [$category->id]) }}">{{ $category->title }}</a></td>
-                        <td>{{ $category->user->name }}</td>
-                        <td>
-
-                            {{ Form::open(['action' => ['CategoryController@destroy', $category->id], 'method' => 'delete']) }}
-                                <a class="btn btn-default" href="{{ action('CategoryController@edit', [$category->id]) }}">Bearbeiten</a>
-                                {{ Form::submit('Löschen', ['class'=>'btn btn-danger']) }}
-                            {{ Form::close() }}
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-            @else
-                Es sind noch keine Kategorien vorhanden. Füge jetzt eine hinzu:<br>
-                <a class="btn btn-primary" href="{{ action('CategoryController@create') }}">Kategorie erstellen</a>
-            @endif
+                                    {{ Form::open(['action' => ['CategoryController@destroy', $category->id], 'method' => 'delete']) }}
+                                    <a class="btn btn-default"
+                                       href="{{ action('CategoryController@edit', [$category->id]) }}"
+                                       data-toggle="tooltip" data-placement="left"
+                                       title="Gibt die Möglichkeit den Titel der Kategorie zu ändern.">Kategorietitel
+                                        ändern</a>
+                                    {{ Form::submit('Löschen', ['class'=>'btn btn-danger', 'data-toggle' => 'tooltip', 'data-placement' => 'left', 'title' => 'Löscht die ausgewählte Kategorie']) }}
+                                    {{ Form::close() }}
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                @else
+                    Es sind noch keine Kategorien vorhanden. Füge jetzt eine hinzu:<br>
+                    <a class="btn btn-primary" href="{{ action('CategoryController@create') }}" data-toggle="tooltip"
+                       data-placement="left" title="Erstellt eine neue Kategorie.">Kategorie erstellen</a>
+                @endif
+            </div>
         </div>
     </div>
-</div>
+
+    <script>
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip();
+        })
+    </script>
 
 @endsection
