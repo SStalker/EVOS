9d0ef6dad30b0c42bff9589b98e99144c9af7947
Christian Wansart <cwansart@users.noreply.github.com>
2016-07-05 23:57:04 +0200

    Implemented add image feature. Closed #137

--- resources/views/questions/_form.blade.php
+++ resources/views/questions/_form.blade.php
@@ -1,19 +1,23 @@
 <div class="panel-body">
     <div class="form-group">
         <label for="question">Frage</label>
-            {{ Form::textarea('question', null, ['id' => 'questionInput', 'class' => 'form-control', 'rows' => 3]) }} <br>
-        <a class="btn btn-primary preview" style="margin-top: -7px;" href="" data-toggle="modal" data-target="#previewBox" data-preview="questionInput">Vorschau</a>
-        <label class="btn btn-default" for="file-selector" style="margin-top:-7px">
-            <input id="file-selector" type="file" style="display:none;">
+        {{ Form::textarea('question', null, ['id' => 'questionInput', 'class' => 'form-control', 'rows' => 3]) }} <br>
+        <a class="btn btn-primary preview" style="margin-top: -7px;" href="" data-toggle="modal"
+           data-target="#previewBox" data-preview="questionInput">Vorschau</a>
+        <label class="btn btn-default" for="file-selector-question" style="margin-top:-7px">
+            <input id="file-selector-question" name="file-selector-question" type="file" class="image-selector"
+                   style="display:none;" data-target="question">
             Bild anhängen
         </label>
     </div>
     <div class="form-group">
         <label for="answerA">Antwort 1</label>
-            {{ Form::text('answerA', null, ['id' => 'answerA', 'class' => 'form-control']) }}<br>
-        <a class="btn btn-primary preview" style="margin-top: -7px;" href="" data-toggle="modal" data-target="#previewBox" data-preview="answerA">Vorschau</a>
-        <label class="btn btn-default" for="file-selector" style="margin-top:-7px">
-            <input id="file-selector" type="file" style="display:none;">
+        {{ Form::text('answerA', null, ['id' => 'answerA', 'class' => 'form-control']) }}<br>
+        <a class="btn btn-primary preview" style="margin-top: -7px;" href="" data-toggle="modal"
+           data-target="#previewBox" data-preview="answerA">Vorschau</a>
+        <label class="btn btn-default" for="file-selector-answer-a" style="margin-top:-7px">
+            <input id="file-selector-answer-a" type="file" class="image-selector" style="display:none;"
+                   data-target="answerA">
             Bild anhängen
         </label>
         <div class="pull-right" style="margin-top:-7px;">
@@ -22,10 +26,12 @@
     </div>
     <div class="form-group">
         <label for="answerB">Antwort 2</label>
-            {{ Form::text('answerB', null, ['id' => 'answerB','class' => 'form-control']) }}<br>
-        <a class="btn btn-primary preview" style="margin-top: -7px;" href="" data-toggle="modal" data-target="#previewBox" data-preview="answerB">Vorschau</a>
-        <label class="btn btn-default" for="file-selector" style="margin-top:-7px">
-            <input id="file-selector" type="file" style="display:none;">
+        {{ Form::text('answerB', null, ['id' => 'answerB','class' => 'form-control']) }}<br>
+        <a class="btn btn-primary preview" style="margin-top: -7px;" href="" data-toggle="modal"
+           data-target="#previewBox" data-preview="answerB">Vorschau</a>
+        <label class="btn btn-default" for="file-selector-answer-b" style="margin-top:-7px">
+            <input id="file-selector-answer-b" type="file" class="image-selector" style="display:none;"
+                   data-target="answerB">
             Bild anhängen
         </label>
         <div class="pull-right" style="margin-top:-7px;">
@@ -34,10 +40,12 @@
     </div>
     <div class="form-group">
         <label for="answerC">Antwort 3</label>
-            {{ Form::text('answerC', null, ['id' => 'answerC', 'class' => 'form-control']) }}<br>
-        <a class="btn btn-primary preview" style="margin-top: -7px;" href="" data-toggle="modal" data-target="#previewBox" data-preview="answerC">Vorschau</a>
-        <label class="btn btn-default" for="file-selector" style="margin-top:-7px">
-            <input id="file-selector" type="file" style="display:none;">
+        {{ Form::text('answerC', null, ['id' => 'answerC', 'class' => 'form-control']) }}<br>
+        <a class="btn btn-primary preview" style="margin-top: -7px;" href="" data-toggle="modal"
+           data-target="#previewBox" data-preview="answerC">Vorschau</a>
+        <label class="btn btn-default" for="file-selector-answer-c" style="margin-top:-7px">
+            <input id="file-selector-answer-c" type="file" class="image-selector" style="display:none;"
+                   data-target="answerC">
             Bild anhängen
         </label>
         <div class="pull-right" style="margin-top:-7px;">
@@ -46,10 +54,12 @@
     </div>
     <div class="form-group">
         <label for="answerD">Antwort 4</label>
-            {{ Form::text('answerD', null, ['id' => 'answerD', 'class' => 'form-control']) }}<br>
-        <a class="btn btn-primary preview" style="margin-top: -7px;" href="" data-toggle="modal" data-target="#previewBox" data-preview="answerD">Vorschau</a>
-        <label class="btn btn-default" for="file-selector" style="margin-top:-7px">
-            <input id="file-selector" type="file" style="display:none;">
+        {{ Form::text('answerD', null, ['id' => 'answerD', 'class' => 'form-control']) }}<br>
+        <a class="btn btn-primary preview" style="margin-top: -7px;" href="" data-toggle="modal"
+           data-target="#previewBox" data-preview="answerD">Vorschau</a>
+        <label class="btn btn-default" for="file-selector-answer-d" style="margin-top:-7px">
+            <input id="file-selector-answer-d" type="file" class="image-selector" style="display:none;"
+                   data-target="answerD">
             Bild anhängen
         </label>
         <div class="pull-right" style="margin-top:-7px;">
@@ -72,7 +82,8 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title" id="myModalLabel">Vorschau</h4>
             </div>
             <div class="modal-body">
@@ -92,15 +103,15 @@
         var isToggled = false;
 
         //Check if any answer is toggled right
-        $.each($('.toggle.btn input[type=checkbox]'), function() {
+        $.each($('.toggle.btn input[type=checkbox]'), function () {
 
             //If checkbox is checked
-            if($(this).prop('checked')){
+            if ($(this).prop('checked')) {
                 //Get id of answer for this checkbox
                 var answer = $(this).attr('data-id');
 
                 //If answer of checkbox is not empty
-                if($('#'+answer).val() !== ""){
+                if ($('#' + answer).val() !== "") {
                     isToggled = true;
                 }
 
@@ -108,18 +119,18 @@
         });
 
         //Enable or disable the button
-        if(answerA != "" && answerB != "" && isToggled){
+        if (answerA != "" && answerB != "" && isToggled) {
             document.getElementById("formSubmit").disabled = false;
             // delete tooltip
             $('#formSubmit').attr('title', '');
-        }else {
+        } else {
             document.getElementById("formSubmit").disabled = true;
             // set tooltip
             $('#formSubmit').attr('title', 'Nicht genügend Eingabedaten vorhanden.');
         }
     }
 
-    $( document ).ready(function() {
+    $(document).ready(function () {
 
         $("#questionInput").focus();
 
@@ -132,14 +143,54 @@
         $("input[id^='answer']").on('keyup', enableSubmit);
         $('.toggle.btn input[type=checkbox]').on('change', enableSubmit);
 
-        $(".preview").click(function(){
+        $(".preview").click(function () {
             var id = $(this).data('preview');
             var text = $("#" + id).val();
-            $(".modal-body").text(text);
+            console.log(text);
+
+            var images = text.match(/\[image\((\d+\.[A-Za-z]{1,4})\)\]/g);
+            images.forEach(function (image) {
+                var matches = image.match(/.*\((\d+\.[A-Za-z]{1,4})\).*/);
+                var html = '<img src="{{ asset('storage/uploads/') }}/' + matches[1] + '">';
+                text = text.replace(matches[0], html);
+            });
+            $(".modal-body").html(text);
 
             window.setTimeout(function () {
                 MathJax.Hub.Typeset();
             }, 1000);
+        });
+
+        $('.image-selector').on('change', function () {
+            if ($(this).hasClass('disabled')) {
+                return;
+            }
+
+            $(this).parent().addClass('disabled');
+            var fd = new FormData();
+            fd.append('image', $(this)[0].files[0]);
+
+            var that = this;
+            $.ajax({
+                url: '{{ action('ImageController@store') }}',
+                data: fd,
+                processData: false,
+                contentType: false,
+                type: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                },
+                success: function (filename) {
+                    console.log('#' + $(that).data('target'));
+                    var imageCode = '[image(' + filename + ')]';
+                    $('[name=' + $(that).data('target') + ']').val($('[name=' + $(that).data('target') + ']').val() + imageCode);
+                    $(that).parent().removeClass('disabled');
+                },
+                fail: function () {
+                    alert('Bild konnte nicht hochgeladen werden. Bitte an einen Administrator wenden!');
+                    $(that).parent().removeClass('disabled');
+                }
+            });
         });
     });
 </script>
--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -14,7 +14,7 @@
             <span class="glyphicon glyphicon-info-sign" aria-hidden="true" data-toggle="popover" title="Formeln einbinden" data-content="EVOS unterstützt LaTeX und AsciiMath. Nutzen Sie für LaTeX $$[Formel]$$, für AsciiMath ´[Formel]´. " style="cursor:pointer"></span>
         </div>
 
-        {{ Form::open(['action' => ['QuestionController@store', $quiz->id], 'method' => 'post']) }}
+        {{ Form::open(['action' => ['QuestionController@store', $quiz->id], 'method' => 'post', 'id' => 'question-form']) }}
             @include('questions._form', ['submitLabel' => 'Speichern'])
         {{ Form::close() }}
     </div>
